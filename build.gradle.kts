9a8a7277a5558172de511e7d7d895e38625066db
Martin Gropp <martin@eidu.com>
2024-10-15 10:05:02 +0200

    ⬆️  kotlin: update to 2.0.20 - KOCHA-1924

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon
 
 plugins {
-    kotlin("multiplatform") version "1.9.22"
+    kotlin("multiplatform") version "2.0.20"
     `maven-publish`
 }
 
